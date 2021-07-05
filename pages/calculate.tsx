f910490ca0663999eaf7152af980f3a9ca5f532a
Kary <karyma101@hotmail.com>
2021-07-05 15:15:15 -0700

    implement auth0

---
+++ pages/calculate.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Calculate() {
+  return (
+    <>
+      <a href="/api/auth/logout">Logout</a>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React from "react";
+import { styled } from "styled-components";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>meFit</h1>
+          <a href="/api/auth/login">Login</a>
+        </nav>
+
+        <div>
+          <form></form>
+          <h2>Sign up for free calculate and track your workouts</h2>
+        </div>
+        {/* SVG */}
+      </header>
+    </>
+  );
 }
