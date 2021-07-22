66700a905b0ead7d4cbdb9e1b35949c0ea642460
Kary <karyma101@hotmail.com>
2021-07-21 19:04:24 -0700

    Added global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,6 @@ import { AppProps } from 'next/app'
 import { createGlobalStyle } from 'styled-components'
 import { UserProvider } from '@auth0/nextjs-auth0'
 
-const GlobalStyle = createGlobalStyle`
-  
-`
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
@@ -15,5 +11,47 @@ function MyApp({ Component, pageProps }: AppProps) {
     </UserProvider>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+`
 
 export default MyApp
