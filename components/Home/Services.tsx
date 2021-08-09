6456446bca999fb45426cfd2fe4307dae5bbd0c0
Kary <karyma101@hotmail.com>
2021-08-09 14:20:23 -0700

    Update responsiveness for Service.tsx

--- components/Home/Services.tsx
+++ components/Home/Services.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import Theme from '../../utils'
 
 import Button from '../common/Button'
 
@@ -10,22 +9,34 @@ export default function Services() {
             <H3>Our Services</H3>
             <Container>
                 <Card color='white'>
-                    <CardTitle>Up to date research</CardTitle>
-                    <CardText>MeFit offers software solution for your workout needs with the latest research. Tracks RPE(Rate of Perceived Exertion) and total volume. MeFit offers software solution for your workout needs with the latest research. Tracks RPE(Rate of Perceived Exertion) and total volume.
-                    </CardText>
+                    <Img src='1.jpg' />
+                    <CardTextContainer>
+                        <h4>Up to date research</h4>
+                        <CardUnderline />
+                        <p>
+                            MeFit offers software solution for your workout needs with the latest research. Tracks RPE(Rate of Perceived Exertion) and total volume. MeFit offers software solution for your workout needs with the latest research. Tracks RPE(Rate of Perceived Exertion) and total volume.
+                        </p>
+                    </CardTextContainer>
                 </Card>
                 <Card>
-                    <CardTitle>Track your progress</CardTitle>
-                    <CardText style={{ marginBottom: '30px' }}>
-                        MeFit offers an easy way for you to track and calculate your progress. Gives you a way to see your progression with clean and visually appealing graphs. MeFit offers an easy way for you to track and calculate your progress. Gives you a way to see your progression with clean and visually appealing graphs.
-                    </CardText>
-                    <Button text="Calculate" />
+                    <Img src='2.jpg' />
+                    <CardTextContainer>
+                        <h4>Track your progress</h4>
+                        <CardUnderline />
+                        <p>
+                            MeFit offers an easy way for you to track and calculate your progress. Gives you a way to see your progression with clean and visually appealing graphs. MeFit offers an easy way for you to track and calculate your progress. Gives you a way to see your progression with clean and visually appealing graphs.
+                        </p>
+                    </CardTextContainer>
                 </Card>
                 <Card color='white'>
-                    <CardTitle>Access Anywhere</CardTitle>
-                    <CardText>
-                        Calculate and see your progress anywhere. Coming to apple and android soon. Calculate and see your progress anywhere. Coming to apple and android soon.
-                    </CardText>
+                    <Img src='3.jpg' />
+                    <CardTextContainer>
+                        <h4>Access Anywhere</h4>
+                        <CardUnderline />
+                        <p>
+                            Calculate and see your progress anywhere. Coming to apple and android soon. Calculate and see your progress anywhere. Coming to apple and android soon.
+                        </p>
+                    </CardTextContainer>
                 </Card>
             </Container>
         </Wrapper>
@@ -33,41 +44,61 @@ export default function Services() {
 }
 
 const Wrapper = styled.section`
-    color: ${({ theme }) => theme.primaryTextColor};
-    padding: 20vh 5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color:#060606;
-`
+            color: ${({ theme }) => theme.primaryTextColor};
+            padding: 20vh 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color:#060606;
+            `
 
 const H3 = styled.h3`
-    margin-bottom: 5rem;
-    color: ${({ theme }) => theme.tertiaryTextColor};
-`
+            margin-bottom: 5rem;
+            `
 const Container = styled.div`
-    display: flex;
-    align-items: flex-start;
-`
+            display: flex;
+            width: 100%;
+
+            @media (max-width: 1000px) {
+                flex-direction: column;
+                width: 50%;
+            }
+
+            @media (max-width: 700px) {
+                flex-direction: column;
+                width: 100%;
+            }
+            `
 
 const Card = styled.div`
-    display: inline-flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 2rem;
-    background-color:${({ color }) => color ? color : null};
-    color: ${({ color, theme }) => color ? 'black' : theme.color};
-    text-align: justify;
-    flex: 1;
-`
+            display: inline-flex;
+            flex-direction: column;
+            background-color: white;
+            color: black;
+            text-align: justify;
+            padding-bottom: 1rem;
+            overflow: hidden;
+            flex: 1;
+            margin: 0.5rem;
+            `
 
-const CardTitle = styled.h4`
-    margin-bottom: 1rem;
-`
+const Img = styled.div`
+            background: ${({ src }) => `url('/cards/${src}') no-repeat center center`};
+            background-size: cover;
+            height: 300px;
+            width: 100%;
+            `
 
-const CardText = styled.p`
-    flex-grow: 0;
-`
+const CardTextContainer = styled.div`
+            padding: 1.5rem;
+            `
 
+const CardUnderline = styled.hr`
+            width: 20%;
+            height: 10px;
+            background-color: ${({ theme }) => theme.tertiaryTextColor};
+            border: none;
+            margin: 1rem 0;
+            `
 
